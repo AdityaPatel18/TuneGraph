3edddd51f0853632eaa9ba427fa40643d66bc4e6
Aditya Patel <153152695+AdityaPatel18@users.noreply.github.com>
2025-02-08 13:10:22 -0500

    create seperate datasets

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def copy_file(source, destination):
+    try:
+        shutil.copy(source, destination)
+        print(f"File copied from {source} to {destination}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def reduce_csv_file_inplace(input_file, num_lines):
+    try:
+        with open(input_file, 'r+', newline='', encoding='utf-8') as file:
+            lines = file.readlines()
+            if len(lines) > num_lines:
+                file.seek(0)
+                file.writelines(lines[:num_lines])
+                file.truncate() 
+            else:
+                print("The CSV file has fewer than 500 lines, no truncation needed.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 @app.get("/api/download_dataset")
 async def download_dataset():
@@ -27,6 +46,10 @@ async def download_dataset():
         filename = os.path.basename(temp_path)  # Extract the filename from the path
         final_path = os.path.join(folder_path, filename)  # Destination path in the folder
         shutil.move(temp_path, final_path)
+        input_file = 'data/1/spotify_data.csv'
+        copy_file(input_file,'data/1/graph_data.csv')
+        reduce_csv_file_inplace('data/1/graph_data.csv',250)
+        reduce_csv_file_inplace(input_file, 1000)
     except Exception as e:
       return JSONResponse(status_code=400, content={"message": str(e)})
 
