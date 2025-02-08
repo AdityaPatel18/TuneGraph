79c87b79db9695fd39d00bf2e5a1eca83d4fa2d3
unicornartist <68564687+unicornartist@users.noreply.github.com>
2025-02-08 18:17:23 -0500

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,9 +22,45 @@ export default function Home() {
   };
 
   return (
-    <div style={{ width: "900px", height: "500px" }}>
+    <div style={{ width: "900px", height: "900px" }}>
       <h2>Scatter Plot</h2>
       <Chart chartType="ScatterChart" width="100%" height="400px" data={data} options={options} />
     </div>
   );
 }
+
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Year 1', 20, 28, 38, 45],
+      ['Year 2', 31, 38, 55, 66],
+      ['Year  3', 50, 55, 77, 80],
+      ['Year 4', 77, 77, 66, 50],
+      ['Year 5', 68, 66, 22, 15]
+      // Treat first row as data as well.
+    ], true);
+
+    var options = {
+      legend:'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+  }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
+
+
+
+
